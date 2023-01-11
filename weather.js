80ddc1ef3558e60a6ad2fd069813621712e5e5ce
svyat174 <svyatgluhov@gmail.com>
2023-01-11 15:05:04 +0300

    final realisation weather cli 11.01.23 15-05

--- weather.js
+++ weather.js
@@ -1,19 +1,73 @@
 #!/usr/bin/env node
 import { getArgs } from './helpers/args.js';
-import { printHelp } from './services/log.service.js';
+import { getWeather } from './services/api.service.js';
+import {
+  printHelp,
+  printError,
+  printSuccess,
+  printWeather,
+} from './services/log.service.js';
+import {
+  getKeyValue,
+  saveKeyValue,
+  TOKEN_DICTIONARY,
+} from './services/storage.service.js';
+
+const saveToken = async (token) => {
+  if (!token.length) {
+    printError('Not write token');
+    return;
+  }
+  try {
+    await saveKeyValue(TOKEN_DICTIONARY.token, token);
+    printSuccess('Token saved!');
+  } catch (e) {
+    printError(e.message);
+  }
+};
+
+const saveCity = async (city) => {
+  if (!city.length) {
+    printError('Not write city');
+    return;
+  }
+  try {
+    await saveKeyValue(TOKEN_DICTIONARY.city, city);
+    printSuccess('City saved!');
+  } catch (e) {
+    printError(e.message);
+  }
+};
+
+const getForecast = async () => {
+  try {
+    const city = await getKeyValue(TOKEN_DICTIONARY.city);
+    const weather = await getWeather(city);
+    printWeather(weather);
+  } catch (e) {
+    if (e.response?.status === 404) {
+      printError('Error type city');
+    } else if (e.response?.status === 401) {
+      printError('Error typing token');
+    } else {
+      printError(e.message);
+    }
+  }
+};
 
 const initCLI = () => {
   const args = getArgs(process.argv);
   if (args.h) {
-    printHelp();
+    return printHelp();
   }
   if (args.s) {
-    // save city
+    return saveCity(args.s);
   }
   if (args.t) {
-    // save token
+    return saveToken(args.t);
   }
-  // return weather
+
+  return getForecast();
 };
 
 initCLI();
