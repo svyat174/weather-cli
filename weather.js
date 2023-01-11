7d422615fd64aae52049874a46fd55bce1d39af4
svyat174 <svyatgluhov@gmail.com>
2023-01-11 12:36:03 +0300

    11.01.23 12-35

---
+++ weather.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+import { getArgs } from './helpers/args.js';
+import { printHelp } from './services/log.service.js';
+
+const initCLI = () => {
+  const args = getArgs(process.argv);
+  if (args.h) {
+    printHelp();
+  }
+  if (args.s) {
+    // save city
+  }
+  if (args.t) {
+    // save token
+  }
+  // return weather
+};
+
+initCLI();
